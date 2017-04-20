69a1b2ff469a24b73e54a3ca3a665dd7511cdea4
Bachelor 3 <bachelor3@iMac-10.fro-204.froment.in.iesanetwork.com>
2017-04-20 18:30:56 +0200

    exo 10

--- exo/hello/platforms/ios/www/js/index.js
+++ exo/hello/platforms/ios/www/js/index.js
@@ -26,27 +26,61 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
+
     onDeviceReady: function() {
+        
         this.receivedEvent('deviceready');
         
+        //date
         document.getElementById('timestamp').innerHTML = new Date();
+        
+        //device
         document.getElementById('deviceapi').innerHTML = device.cordova;
         
+        // Type de connexion
         var networkState = navigator.connection.type;
-                   var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.CELL]     = 'Cell generic connection';
-            states[Connection.NONE]     = 'No network connection';
-            
-            alert('Connection type: ' + states[networkState]);
-              
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+        alert('Connection type: ' + states[networkState]);
+        
+        function onSuccess(contacts) {
+            alert('Found ' + contacts.length + ' contacts.');
+        };
+        
+        function onError(contactError) {
+            alert('onError!');
+        };
+        
+        var options      = new ContactFindOptions();
+        options.multiple = true;
+        var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+        
+        document.getElementById('contacts').innerHTML = JSON.stringify(navigator.contacts.find(fields, onSuccess, onError, options));
+
+
+        
+        //local storage
+        
+        
+        //contact
+      /*//var myContact = navigator.contacts.create({"displayName": "Test User"});
+        var myContact = navigator.contacts.create();
+        //contact.displayName = contact.nickname = name;
+        contact.name = new ContactName();
+        contact.name.givenName = name;
+        contact.emails = [];
+        contact.emails[] = new ContactField('PhoneGape', email, true); //preferred
+        contact.save(this.onContactSaved, this.onContactSavedError)
+    */
+        
     },
-    
 
 
     // Update DOM on a Received Event
